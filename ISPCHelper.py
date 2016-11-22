e01adbc5c535a74315c364780b747380d9a75a5d
SergiX44 <sergio@brighenti.me>
2016-11-22 17:33:09 +0100

    removed pma and roundcube install, php compiler

--- ISPCHelper.py
+++ ISPCHelper.py
@@ -8,7 +8,6 @@ __version__ = 1.0
 SUPPORTED_PLATFORMS = ['ubuntu', 'debian', '', 'linuxmint']  # '' and 'linuxmint' for development purposes
 
 INSTALL_PARENT_PATH = '/opt/'
-PHPMYADMIN_INSTALL_PATH = INSTALL_PARENT_PATH + 'phpmyadmin'
 ROUNDCUBE_INSTALL_PATH = INSTALL_PARENT_PATH + 'roundcube'
 PHP_INSTALL_PATH = INSTALL_PARENT_PATH + 'php{0}'
 
@@ -19,56 +18,17 @@ def parse_json(url):
 	return json.loads(data)
 
 
-def install_phpmyadmin():
-	last_release = parse_json('https://api.github.com/repos/phpmyadmin/phpmyadmin/releases/latest')
-	os.system('cd /tmp/ && wget -O pma.tar.gz ' + last_release['tarball_url'] + ' && tar zxf pma.tar.gz -C /opt/')
-	os.system('mv ' + INSTALL_PARENT_PATH + 'phpmyadmin-phpmyadmin-* ' + PHPMYADMIN_INSTALL_PATH)
-	os.system('rm /tmp/pma.tar.gz')
-	
-	apache_conf = '''
-# phpMyAdmin ISPCHelper Apache configuration
-
-Alias /phpmyadmin {0}
-
-<Directory {0}>
-    Options FollowSymLinks
-	Require all granted
-
-    <IfModule mod_fcgid.c>
-            Options +ExecCGI
-            AddHandler fcgid-script .php
-            FCGIWrapper /var/www/php-fcgi-scripts/ispconfig/.php-fcgi-starter .php
-            Order allow,deny
-            allow from all
-    </IfModule>
-</Directory>
-
-<Directory {0}/libraries>
-    Require all denied
-</Directory>
-'''
-	apache_conf.format(PHPMYADMIN_INSTALL_PATH)
-	conf = open('/etc/apache2/conf-enabled/phpmyadmin.conf', 'w')
-	conf.write(apache_conf)
-	conf.close()
-	
-	os.system('service apache2 restart')
-	
-	
-	
-
-def install_roundcube():
-	last_release = parse_json('https://api.github.com/repos/roundcube/roundcubemail/releases/latest')
-	os.system('cd /tmp/ && wget -O roundcube.tar.gz ' + last_release['assets'][2]['browser_download_url'] + ' && tar zxf roundcube.tar.gz -C /opt/')
-	os.system('mv ' + INSTALL_PARENT_PATH + 'roundcubemail-* ' + ROUNDCUBE_INSTALL_PATH)
-	os.system('rm /tmp/roundcube.tar.gz')
-	
-
-
 def install_update_php(version):
-	pass
-
-
+	os.makedirs(PHP_INSTALL_PATH.format(version['name'][0]+version['name'][2])
+	os.makedirs('/usr/local/src/php5-build')
+	
+	os.system('cd /usr/local/src/php5-build && wget {0} -O {1} && tar jxf {1}'.format(version['downloadurl'], os.path.basename(version['downloadurl'])))
+	
+	os.system('apt-get -y install libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev')
+	os.system('ln -s /usr/lib/libc-client.a /usr/lib/x86_64-linux-gnu/libc-client.a')
+	
+	os.system('cd /usr/local/src/php5-build && ./configure --prefix={0} --with-pdo-pgsql --with-zlib-dir --with-freetype-dir --enable-mbstring --with-libxml-dir=/usr --enable-soap --enable-calendar --with-curl --with-mcrypt --with-zlib --with-gd --with-pgsql --disable-rpath --enable-inline-optimization --with-bz2 --with-zlib --enable-sockets --enable-sysvsem --enable-sysvshm --enable-pcntl --enable-mbregex --enable-exif --enable-bcmath --with-mhash --enable-zip --with-pcre-regex --with-pdo-mysql --with-mysqli --with-mysql-sock=/var/run/mysqld/mysqld.sock --with-jpeg-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --with-openssl --with-fpm-user=www-data --with-fpm-group=www-data --with-libdir=/lib/x86_64-linux-gnu --enable-ftp --with-imap --with-imap-ssl --with-kerberos --with-gettext --with-xmlrpc --with-xsl --enable-opcache --enable-fpm'.format(PHP_INSTALL_PATH))
+	
 def php_menu():
 	php_versions = parse_json('https://raw.githubusercontent.com/SergiX44/ISPCHelper/master/php_versions.json')
 
@@ -89,13 +49,9 @@ def php_menu():
 
 def main_menu():
 	print('1) Manage PHP Additional Versions')
-	print('2) Install/update latest phpMyAdmin')
-	print('3) Install/update latest Roundcube')
 
 	menu = {
-		'1': php_menu,
-		'2': install_phpmyadmin,
-		'3': install_roundcube
+		'1': php_menu
 	}
 
 	sel = raw_input('\nChoose an option: ')
