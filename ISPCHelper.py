eb46542312dbbf05d809a2052e394be7da3bdc6a
Sergio Brighenti <sergio@brighenti.me>
2016-12-04 18:09:07 +0100

    Update ISPCHelper.py

--- ISPCHelper.py
+++ ISPCHelper.py
@@ -39,7 +39,7 @@ def install_update_php(version):
 
     print('Dependecies check...')
     os.system(
-        'apt-get -y install libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev')
+        'apt-get -y install build-essential libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev')
     os.system('ln -s /usr/lib/libc-client.a /usr/lib/x86_64-linux-gnu/libc-client.a')
 
     print('Creating configure...')
