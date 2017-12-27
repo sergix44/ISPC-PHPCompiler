68faf34e010f409005e164494ee4b9e726d55207
Sergio Brighenti <sergio@brighenti.me>
2017-12-27 18:11:44 +0100

    check return code after install deps

--- php-compiler.sh
+++ php-compiler.sh
@@ -130,6 +130,7 @@ install_dependencies() {
     if [ "${DISTRO}" == "ubuntu-16.04" ]; then
         apt-get -y install build-essential nano wget libxml2-dev libjpeg62-dbg libfcgi-dev libfcgi0ldbl libjpeg62-turbo-dbg libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libpng12-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev libwebp-dev
     fi
+    check_return_code
 }
 
 show_menu() {
