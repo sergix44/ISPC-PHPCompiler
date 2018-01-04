1ca7bf6209926bf257ab7e9dde6d2ba14c176b62
Pietro Marangon <pietro.marangon@gmail.com>
2018-01-04 18:35:46 +0100

    New version [ci skip]

--- versions.sh
+++ versions.sh
@@ -6,16 +6,16 @@
 # shellcheck disable=SC2034
 declare -A VERSIONS=(
     ["PHP 5.6.32"]="http://php.net/distributions/php-5.6.32.tar.gz"
-    ["PHP 7.0.26"]="http://php.net/distributions/php-7.0.26.tar.gz"
-    ["PHP 7.1.12"]="http://php.net/distributions/php-7.1.12.tar.gz"
-    ["PHP 7.2.0"]="http://php.net/distributions/php-7.2.0.tar.gz"
+    ["PHP 7.0.27"]="http://php.net/distributions/php-7.0.27.tar.gz"
+    ["PHP 7.1.13"]="http://php.net/distributions/php-7.1.13.tar.gz"
+    ["PHP 7.2.1"]="http://php.net/distributions/php-7.2.1.tar.gz"
 )
 
 # All PHP versions checksum
 # shellcheck disable=SC2034
 declare -A CHECKSUM=(
     ["PHP 5.6.32"]="7bef1ae8cd633df5b9c5964262d276d2dc21acbfcd94022d1e2084d199315df4"
-    ["PHP 7.0.26"]="04c345f7c9e3f1cd02f275bfec893a4e0290e724073f2f3d7282a219128b537c"
-    ["PHP 7.1.12"]="188c67d8e424ce7a6fe93475aa64f53182c1d80ca3ac99439651ca91569d969c"
-    ["PHP 7.2.0"]="801876abd52e0dc58a44701344252035fd50702d8f510cda7fdb317ab79897bc"
+    ["PHP 7.0.27"]="809c0181e970dd17c6a6cabbf64518e719c7253e7490f8e1279bc1e1fbdf7996"
+    ["PHP 7.1.13"]="12fcbf59c9eb9af215ef38815d5da39b9d74549092c34b0dfc31442699740ce9"
+    ["PHP 7.2.1"]="8ecb2950571054a00687ccbd023874a4a075ccd1e2ec3dc00fc25ef589a77dba"
 )
