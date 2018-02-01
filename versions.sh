581e665b7a4be64dc36eccf89db4915bca1754de
Sergio Brighenti <sergio@brighenti.me>
2018-02-02 00:44:04 +0100

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -5,17 +5,17 @@
 # All PHP versions
 # shellcheck disable=SC2034
 declare -A VERSIONS=(
-    ["PHP 5.6.32"]="http://php.net/distributions/php-5.6.32.tar.gz"
+    ["PHP 5.6.33"]="http://php.net/distributions/php-5.6.33.tar.gz"
     ["PHP 7.0.27"]="http://php.net/distributions/php-7.0.27.tar.gz"
-    ["PHP 7.1.13"]="http://php.net/distributions/php-7.1.13.tar.gz"
-    ["PHP 7.2.1"]="http://php.net/distributions/php-7.2.1.tar.gz"
+    ["PHP 7.1.14"]="http://php.net/distributions/php-7.1.14.tar.gz"
+    ["PHP 7.2.2"]="http://php.net/distributions/php-7.2.2.tar.gz"
 )
 
 # All PHP versions checksum
 # shellcheck disable=SC2034
 declare -A CHECKSUM=(
-    ["PHP 5.6.32"]="7bef1ae8cd633df5b9c5964262d276d2dc21acbfcd94022d1e2084d199315df4"
+    ["PHP 5.6.33"]="bedfac81cfaa25961812a1aec458c4109411a14991b43a416343ffb830b8da6a"
     ["PHP 7.0.27"]="809c0181e970dd17c6a6cabbf64518e719c7253e7490f8e1279bc1e1fbdf7996"
-    ["PHP 7.1.13"]="12fcbf59c9eb9af215ef38815d5da39b9d74549092c34b0dfc31442699740ce9"
-    ["PHP 7.2.1"]="8ecb2950571054a00687ccbd023874a4a075ccd1e2ec3dc00fc25ef589a77dba"
+    ["PHP 7.1.14"]="8c7360209d255ee46d388bdcd43ef1a2d14b370c331be30ea628ece18a1e7683"
+    ["PHP 7.2.2"]="5963df05fec21927c03fe9f7bf379be2d1eacde6c0f9dcde6143c7133e55abd4"
 )
