39119f54098536db393ef11357d1fa6f4f72da12
Pietro Marangon <pietro@marangon.me>
2018-07-28 11:54:06 +0200

    Update php-compiler.sh

--- php-compiler.sh
+++ php-compiler.sh
@@ -348,7 +348,7 @@ compile() {
         --with-jpeg-dir=/usr --with-png-dir=/usr --enable-gd-native-ttf --with-openssl --with-fpm-user=www-data \
         --with-fpm-group=www-data ${libdir} --enable-ftp --with-imap --with-imap-ssl \
         --with-kerberos --with-gettext --with-xmlrpc ${webp} --with-xsl \
-        --enable-opcache --enable-fpm)
+        --enable-opcache --enable-intl --enable-fpm)
     check_return_code
 
     CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
