6288a10ea44e8f352f4303e18168c8d855dead19
Sergio Brighenti <sergio@brighenti.me>
2018-11-08 20:30:21 +0100

    BOT: Updated versions.sh and travis ci

--- versions.sh
+++ versions.sh
@@ -7,8 +7,8 @@
 declare -A VERSIONS=(
     ["PHP 5.6.38"]="http://php.net/distributions/php-5.6.38.tar.gz"
     ["PHP 7.0.32"]="http://php.net/distributions/php-7.0.32.tar.gz"
-    ["PHP 7.1.23"]="http://php.net/distributions/php-7.1.23.tar.gz"
-    ["PHP 7.2.11"]="http://php.net/distributions/php-7.2.11.tar.gz"
+    ["PHP 7.1.24"]="http://php.net/distributions/php-7.1.24.tar.gz"
+    ["PHP 7.2.12"]="http://php.net/distributions/php-7.2.12.tar.gz"
 )
 
 # All PHP versions checksum
@@ -16,6 +16,6 @@ declare -A VERSIONS=(
 declare -A CHECKSUM=(
     ["PHP 5.6.38"]="3b74d46cd79a45cce90c8059e09d8bd0beeb5de562cbb0cb42f96ff8fa665fd4"
     ["PHP 7.0.32"]="08d13389f611ec55f3b9164347a97e410099238a3dd85946e556a288ce366fbe"
-    ["PHP 7.1.23"]="b839a4de32e6770d10b87c2495c070d09277fe61008804b2992466f0dcc5f0fa"
-    ["PHP 7.2.11"]="180c63a9647c0a50d438b6bd5c7a8e7a11bceee8ad613a59d3ef15151fc158d4"
+    ["PHP 7.1.24"]="1e780b1af3eeb8fba9e5af6205c960184a0c3a0ef091aaa192e7b7d6b67405d0"
+    ["PHP 7.2.12"]="d7cabdf4e51db38121daf0d494dc074743b24b6c79e592037eeedd731f1719dd"
 )
