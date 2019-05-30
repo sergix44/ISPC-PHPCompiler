7f749ce9fab6e0e5befc7a043da3b6b8e4af9c36
Sergio Brighenti <sergio@brighenti.me>
2019-05-30 22:26:28 +0200

    Updated versions.sh and travis ci

--- versions.sh
+++ versions.sh
@@ -7,9 +7,9 @@
 declare -A VERSIONS=(
     ["PHP 5.6.40"]="http://php.net/distributions/php-5.6.40.tar.gz"
     ["PHP 7.0.33"]="http://php.net/distributions/php-7.0.33.tar.gz"
-    ["PHP 7.1.29"]="http://php.net/distributions/php-7.1.29.tar.gz"
-    ["PHP 7.2.18"]="http://php.net/distributions/php-7.2.18.tar.gz"
-    ["PHP 7.3.5"]="http://php.net/distributions/php-7.3.5.tar.gz"
+    ["PHP 7.1.30"]="http://php.net/distributions/php-7.1.30.tar.gz"
+    ["PHP 7.2.19"]="http://php.net/distributions/php-7.2.19.tar.gz"
+    ["PHP 7.3.6"]="http://php.net/distributions/php-7.3.6.tar.gz"
 )
 
 # All PHP versions checksum
@@ -17,7 +17,7 @@ declare -A VERSIONS=(
 declare -A CHECKSUM=(
     ["PHP 5.6.40"]="56fb9878d12fdd921f6a0897e919f4e980d930160e154cbde2cc6d9206a27cac"
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
-    ["PHP 7.1.29"]="bdd0e1707100c8b87f1be516f5b95a26e3eb4114d4316eaf0663bf292ead35bb"
-    ["PHP 7.2.18"]="48aeb291814f3cd3ba03c52e79e8e61896d0271fd4c228198f80a072e568f84b"
-    ["PHP 7.3.5"]="c953749b7f3310a3a74f920ef698f6d1c04636d11656ac9ffb3ab10d34e30e1e"
+    ["PHP 7.1.30"]="a604edf85d5dfc28e6ff3016dad3954c50b93db69afc42295178b4fdf42e026c"
+    ["PHP 7.2.19"]="1cd2266a058f3224d3cba594540045542606996f026eeef96747f27f6b3d22b6"
+    ["PHP 7.3.6"]="72fbf223ff8659a61eed08eebffb4ede0256e7a69d2151ae24affa5377b70bb8"
 )
