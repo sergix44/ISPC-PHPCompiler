ad987a93f02b4c1e94ff08cfd2e5193ab8b1e700
Pietro Marangon <pietro.marangon@gmail.com>
2019-08-13 12:02:25 +0200

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -17,7 +17,7 @@ declare -A VERSIONS=(
 declare -A CHECKSUM=(
     ["PHP 5.6.40"]="56fb9878d12fdd921f6a0897e919f4e980d930160e154cbde2cc6d9206a27cac"
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
-    ["PHP 7.1.31"]="a604edf85d5dfc28e6ff3016dad3954c50b93db69afc42295178b4fdf42e026c"
+    ["PHP 7.1.31"]="ea0558735653b9ce63e9cea41dd8f0d0b90dba6c39d39dd9a6aad5cc58b0bdfc"
     ["PHP 7.2.21"]="8327682bee4a8fd2edf5bbfcc393d986b945bec433fc74458d05e766701b313c"
     ["PHP 7.3.8"]="31af3eff3337fb70733c9b02a3444c3dae662ecab20aeec7fdc3c42e22071490"
 )
