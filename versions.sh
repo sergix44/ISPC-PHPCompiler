f5fe95a17eb70fac843c3b3c4922d2a79560de9e
Pietro Marangon <pietro.marangon@gmail.com>
2019-08-13 10:52:22 +0200

    Version [ci skip]

--- versions.sh
+++ versions.sh
@@ -7,9 +7,9 @@
 declare -A VERSIONS=(
     ["PHP 5.6.40"]="http://php.net/distributions/php-5.6.40.tar.gz"
     ["PHP 7.0.33"]="http://php.net/distributions/php-7.0.33.tar.gz"
-    ["PHP 7.1.30"]="http://php.net/distributions/php-7.1.30.tar.gz"
-    ["PHP 7.2.20"]="http://php.net/distributions/php-7.2.20.tar.gz"
-    ["PHP 7.3.7"]="http://php.net/distributions/php-7.3.7.tar.gz"
+    ["PHP 7.1.31"]="http://php.net/distributions/php-7.1.31.tar.gz"
+    ["PHP 7.2.21"]="http://php.net/distributions/php-7.2.21.tar.gz"
+    ["PHP 7.3.8"]="http://php.net/distributions/php-7.3.8.tar.gz"
 )
 
 # All PHP versions checksum
@@ -17,7 +17,7 @@ declare -A VERSIONS=(
 declare -A CHECKSUM=(
     ["PHP 5.6.40"]="56fb9878d12fdd921f6a0897e919f4e980d930160e154cbde2cc6d9206a27cac"
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
-    ["PHP 7.1.30"]="a604edf85d5dfc28e6ff3016dad3954c50b93db69afc42295178b4fdf42e026c"
-    ["PHP 7.2.20"]="d1dbf6f299514c9aa55b2995928b798b27c21811a0447f0688993cdf36be0749"
-    ["PHP 7.3.7"]="4230bbc862df712b013369de94b131eddea1e5e946a8c5e286b82d441c313328"
+    ["PHP 7.1.31"]="a604edf85d5dfc28e6ff3016dad3954c50b93db69afc42295178b4fdf42e026c"
+    ["PHP 7.2.21"]="8327682bee4a8fd2edf5bbfcc393d986b945bec433fc74458d05e766701b313c"
+    ["PHP 7.3.8"]="31af3eff3337fb70733c9b02a3444c3dae662ecab20aeec7fdc3c42e22071490"
 )
