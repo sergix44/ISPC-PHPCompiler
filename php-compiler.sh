6a26c57b2611eebc0cd7ee7674680078eab48e37
Pietro Marangon <pietro.marangon@gmail.com>
2019-08-13 18:52:57 +0200

    Update php-compiler.sh

--- php-compiler.sh
+++ php-compiler.sh
@@ -26,7 +26,7 @@ install_utils() {
         yum -y install whiptail curl wget
     else
         apt-get update && apt-get -y upgrade
-        apt-get -y install whiptail curl wget insserv
+        apt-get -y install whiptail curl wget
     fi
     check_return_code
 }
@@ -147,7 +147,7 @@ install_dependencies() {
     fi
 
     if [ "${DISTRO}" == "debian9" ]; then
-        apt-get -y install build-essential autoconf libfcgi-dev libfcgi0ldbl libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev libwebp-dev libvpx-dev libc-client2007e-dev libicu-dev libzip-dev
+        apt-get -y install build-essential autoconf libfcgi-dev libfcgi0ldbl libmcrypt-dev libssl-dev libc-client2007e libc-client2007e-dev libxml2-dev libbz2-dev libcurl4-openssl-dev libjpeg-dev libfreetype6-dev libkrb5-dev libpq-dev libxml2-dev libxslt1-dev libwebp-dev libvpx-dev libc-client2007e-dev libicu-dev libzip-dev insserv
         check_return_code
         ln -s  /usr/include/x86_64-linux-gnu/curl  /usr/include/curl
         ln -s /usr/lib/libc-client.a /usr/lib/x86_64-linux-gnu/libc-client.a
