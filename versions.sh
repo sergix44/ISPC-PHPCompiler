1c3ee88288dcd758991835b107e8e4dfb691f19f
Sergio Brighenti <sergio@brighenti.me>
2019-11-28 11:41:11 +0100

    php 7.4 [ci skip]

--- versions.sh
+++ versions.sh
@@ -5,12 +5,12 @@
 # All PHP versions
 # shellcheck disable=SC2034
 declare -A VERSIONS=(
-    ["PHP 5.6.40"]="http://php.net/distributions/php-5.6.40.tar.gz"
-    ["PHP 7.0.33"]="http://php.net/distributions/php-7.0.33.tar.gz"
-    ["PHP 7.1.33"]="http://php.net/distributions/php-7.1.33.tar.gz"
-    ["PHP 7.2.24"]="http://php.net/distributions/php-7.2.24.tar.gz"
-    ["PHP 7.3.11"]="http://php.net/distributions/php-7.3.11.tar.gz"
-    ["PHP 7.4.b"]="https://downloads.php.net/~derick/php-7.4.0RC4.tar.gz"
+    ["PHP 5.6.40"]="https://www.php.net/distributions/php-5.6.40.tar.gz"
+    ["PHP 7.0.33"]="https://www.php.net/distributions/php-7.0.33.tar.gz"
+    ["PHP 7.1.33"]="https://www.php.net/distributions/php-7.1.33.tar.gz"
+    ["PHP 7.2.25"]="https://www.php.net/distributions/php-7.2.25.tar.gz"
+    ["PHP 7.3.12"]="https://www.php.net/distributions/php-7.3.12.tar.gz"
+    ["PHP 7.4.0"]="https://www.php.net/distributions/php-7.4.0.tar.gz"
 )
 
 # All PHP versions checksum
@@ -19,7 +19,7 @@ declare -A CHECKSUM=(
     ["PHP 5.6.40"]="56fb9878d12fdd921f6a0897e919f4e980d930160e154cbde2cc6d9206a27cac"
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
     ["PHP 7.1.33"]="0055f368ffefe51d5a4483755bd17475e88e74302c08b727952831c5b2682ea2"
-    ["PHP 7.2.24"]="01baf7a34c856d2c552121fbad7296a8cde18389ce83db32f18252bc1cee4dd6"
-    ["PHP 7.3.11"]="8f385f5bdf9193791f6c0f6303f518f3c324b6655ac108fdb3c426da7f3cf4d4"
-    ["PHP 7.4.b"]="7cc25051b62266b7a473d69f6afb44c69206700c706f64bf8eb33dbc6c347db2"
+    ["PHP 7.2.25"]="b2cb1bd46454d33b2c65c2fd559f464cd14e57dd47b953adf5caa77fdf0de52b"
+    ["PHP 7.3.12"]="d617e5116f8472a628083f448ebe4afdbc4ac013c9a890b08946649dcbe61b34"
+    ["PHP 7.4.0"]="bf206be96a39e643180013df39ddcd0493966692a2422c4b7d3355b6a15a01c0"
 )
