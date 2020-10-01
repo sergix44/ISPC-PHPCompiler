f5fec1a539ba50ff433e36da34983ea945f77788
Pietro Marangon <pietro@marangon.me>
2020-10-01 18:46:48 +0200

    New version [ci skip][skip ci]

--- versions.sh
+++ versions.sh
@@ -8,9 +8,9 @@ declare -A VERSIONS=(
     ["PHP 5.6.40"]="https://www.php.net/distributions/php-5.6.40.tar.gz"
     ["PHP 7.0.33"]="https://www.php.net/distributions/php-7.0.33.tar.gz"
     ["PHP 7.1.33"]="https://www.php.net/distributions/php-7.1.33.tar.gz"
-    ["PHP 7.2.29"]="https://www.php.net/distributions/php-7.2.29.tar.gz"
-    ["PHP 7.3.16"]="https://www.php.net/distributions/php-7.3.16.tar.gz"
-    ["PHP 7.4.4"]="https://www.php.net/distributions/php-7.4.4.tar.gz"
+    ["PHP 7.2.34"]="https://www.php.net/distributions/php-7.2.34.tar.gz"
+    ["PHP 7.3.23"]="https://www.php.net/distributions/php-7.3.23.tar.gz"
+    ["PHP 7.4.11"]="https://www.php.net/distributions/php-7.4.11.tar.gz"
 )
 
 # All PHP versions checksum
@@ -19,7 +19,7 @@ declare -A CHECKSUM=(
     ["PHP 5.6.40"]="56fb9878d12fdd921f6a0897e919f4e980d930160e154cbde2cc6d9206a27cac"
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
     ["PHP 7.1.33"]="0055f368ffefe51d5a4483755bd17475e88e74302c08b727952831c5b2682ea2"
-    ["PHP 7.2.29"]="ea5c96309394a03a38828cc182058be0c09dde1f00f35809622c2d05c50ee890"
-    ["PHP 7.3.16"]="a01ae4f6baf427413c28f8cfddbae86aeff61cdb88658e75404f2d93d98e3255"
-    ["PHP 7.4.4"]="1581b3e10c7854597e1086937d5753cdf92d132865c06a50aed4f4f407138616"
+    ["PHP 7.2.34"]="8b2777c741e83f188d3ca6d8e98ece7264acafee86787298fae57e05d0dddc78"
+    ["PHP 7.3.23"]="a21094b9ba2d8fe7fa5838e6566e30cf4bfaf2c8a6dce90ff707c45d0d8d494d"
+    ["PHP 7.4.11"]="b4fae5c39ca1eedf5597071996d9c85d0674b83f5003126c39b7b44bbfbcd821"
 )
