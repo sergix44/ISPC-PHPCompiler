dcb75762da9f868c77556ffdd40fa761174897ee
Pietro Marangon <pietro@marangon.me>
2020-11-10 11:13:21 +0100

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -9,8 +9,8 @@ declare -A VERSIONS=(
     ["PHP 7.0.33"]="https://www.php.net/distributions/php-7.0.33.tar.gz"
     ["PHP 7.1.33"]="https://www.php.net/distributions/php-7.1.33.tar.gz"
     ["PHP 7.2.34"]="https://www.php.net/distributions/php-7.2.34.tar.gz"
-    ["PHP 7.3.23"]="https://www.php.net/distributions/php-7.3.23.tar.gz"
-    ["PHP 7.4.11"]="https://www.php.net/distributions/php-7.4.11.tar.gz"
+    ["PHP 7.3.24"]="https://www.php.net/distributions/php-7.3.24.tar.gz"
+    ["PHP 7.4.12"]="https://www.php.net/distributions/php-7.4.12.tar.gz"
     ["PHP 8.0.RC3"]="https://downloads.php.net/~carusogabriel/php-8.0.0RC3.tar.gz"
 )
 
@@ -21,7 +21,7 @@ declare -A CHECKSUM=(
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
     ["PHP 7.1.33"]="0055f368ffefe51d5a4483755bd17475e88e74302c08b727952831c5b2682ea2"
     ["PHP 7.2.34"]="8b2777c741e83f188d3ca6d8e98ece7264acafee86787298fae57e05d0dddc78"
-    ["PHP 7.3.23"]="a21094b9ba2d8fe7fa5838e6566e30cf4bfaf2c8a6dce90ff707c45d0d8d494d"
-    ["PHP 7.4.11"]="b4fae5c39ca1eedf5597071996d9c85d0674b83f5003126c39b7b44bbfbcd821"
+    ["PHP 7.3.24"]="ac06577e2aeb69d4bed3c1532ed84a548f01399e5481c144c3e61d146be8ced6"
+    ["PHP 7.4.12"]="f056d74409a71f17218f76538c6a2d7b59ee99db9db7685fa0ab9cd0d4c0f286"
     ["PHP 8.0.RC3"]="c651a10058840abd44f99a8aed0446f102291d1408f2299790b919634689b5cd"
 )
