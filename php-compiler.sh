19db5f7e5c2e13b7a11e2f56a70d6a17760ed19e
Sergio Brighenti <sergio@brighenti.me>
2021-01-08 10:02:13 +0100

    test

--- php-compiler.sh
+++ php-compiler.sh
@@ -489,7 +489,7 @@ compile() {
         ${jpg} --with-png-dir=/usr --with-openssl --with-fpm-user=www-data \
         --with-fpm-group=www-data ${libdir} --enable-ftp --with-imap --with-imap-ssl \
         --with-kerberos --with-gettext --with-xmlrpc ${webp} --with-xsl \
-        --enable-opcache --enable-intl --enable-fpm)
+        --enable-opcache --enable-intl --enable-fpm --with-pear)
     check_return_code
 
     CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
