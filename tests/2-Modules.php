d658ff81dae4591344704a9905416d53743b4af1
Pietro Marangon <pietro@marangon.me>
2021-01-20 20:38:34 +0100

    Update 2-Modules.php

--- tests/2-Modules.php
+++ tests/2-Modules.php
@@ -52,14 +52,20 @@ $modules = [
     'Zend OPcache',
     'zip',
     'zlib',
+    'gmp'
 ];
 
 $e = 0;
 
 foreach($modules as $module){
     if(!extension_loaded ( $module )){
-        echo $module.' not loaded';
-        $e = 1;
+        if (version_compare(PHP_VERSION, '7.0.0') < 0 && $module == 'gmp') {
+            // gmp not enable on PHP 5.6 ubuntu-16.04
+            echo $module.' not loaded but error not triggered';
+        }else{
+            echo $module.' not loaded';
+            $e = 1;
+        }
     }
 }
 
