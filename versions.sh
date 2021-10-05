bcbb9a89e93e1611a9caa9a6abe5ce3353b8abab
Pietro Marangon <pietro@marangon.me>
2021-10-05 19:23:59 +0200

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -9,7 +9,7 @@ declare -A VERSIONS=(
     ["PHP 7.0.33"]="https://www.php.net/distributions/php-7.0.33.tar.gz"
     ["PHP 7.1.33"]="https://www.php.net/distributions/php-7.1.33.tar.gz"
     ["PHP 7.2.34"]="https://www.php.net/distributions/php-7.2.34.tar.gz"
-    ["PHP 7.3.29"]="https://www.php.net/distributions/php-7.3.29.tar.gz"
+    ["PHP 7.3.31"]="https://www.php.net/distributions/php-7.3.31.tar.gz"
     ["PHP 7.4.21"]="https://www.php.net/distributions/php-7.4.21.tar.gz"
     ["PHP 8.0.8"]="https://www.php.net/distributions/php-8.0.8.tar.gz"
 )
@@ -21,7 +21,7 @@ declare -A CHECKSUM=(
     ["PHP 7.0.33"]="d71a6ecb6b13dc53fed7532a7f8f949c4044806f067502f8fb6f9facbb40452a"
     ["PHP 7.1.33"]="0055f368ffefe51d5a4483755bd17475e88e74302c08b727952831c5b2682ea2"
     ["PHP 7.2.34"]="8b2777c741e83f188d3ca6d8e98ece7264acafee86787298fae57e05d0dddc78"
-    ["PHP 7.3.29"]="ba4de3955b0cbd33baee55a83568acc4347605e210a54b5654e4c1e09b544659"
+    ["PHP 7.3.31"]="57ca37b08d3eed4cadc3976e78b0f51d0305bb6e60333f6e8c76e8aee07c3f0f"
     ["PHP 7.4.21"]="4b9623accbe4b8923a801212f371f784069535009185e7bf7e4dec66bbea61db"
     ["PHP 8.0.8"]="084a1e8020e86fb99b663d195fd9ac98a9f37dfcb9ecb5c159054cdb8f388945"
 )
