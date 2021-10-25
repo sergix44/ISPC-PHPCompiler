7fb5a28aeae1ba4f58fb9dbb9a3df0bb59eeb5f6
smanu85 <smanu85@gmail.com>
2021-10-25 23:20:48 +0200

    Updated php 8.0 to latest url and checksum

--- versions.sh
+++ versions.sh
@@ -11,7 +11,7 @@ declare -A VERSIONS=(
     ["PHP 7.2.34"]="https://www.php.net/distributions/php-7.2.34.tar.gz"
     ["PHP 7.3.31"]="https://www.php.net/distributions/php-7.3.31.tar.gz"
     ["PHP 7.4.25"]="https://www.php.net/distributions/php-7.4.25.tar.gz"
-    ["PHP 8.0.11"]="https://www.php.net/distributions/php-8.0.11.tar.gz"
+    ["PHP 8.0.12"]="https://www.php.net/distributions/php-8.0.12.tar.gz"
 )
 
 # All PHP versions checksum
@@ -23,5 +23,5 @@ declare -A CHECKSUM=(
     ["PHP 7.2.34"]="8b2777c741e83f188d3ca6d8e98ece7264acafee86787298fae57e05d0dddc78"
     ["PHP 7.3.31"]="57ca37b08d3eed4cadc3976e78b0f51d0305bb6e60333f6e8c76e8aee07c3f0f"
     ["PHP 7.4.25"]="3b2632252c933cac489a20f68b8f4ab769e5a0a3bf22b6ef47427aff6922e31f"
-    ["PHP 8.0.11"]="c6a461f57b4bcb46cd4dec443253b1e2e8e981466f1280093322b7864afe8be7"
+    ["PHP 8.0.12"]="a5b78f04a89d3b401465febf449c7ea9de48681f92803dd8dc2bf922812d572b"
 )
