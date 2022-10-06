6a4ebf3e52506b3db64aad235a823e80168771c9
Pietro Marangon <pietro@marangon.me>
2022-10-06 12:00:11 +0200

    Update php-compiler.sh

--- php-compiler.sh
+++ php-compiler.sh
@@ -525,7 +525,7 @@ compile() {
         --enable-pcntl --enable-mbregex --enable-exif --enable-bcmath --with-mhash \
         ${zip} --with-pcre-regex --with-pdo-mysql --with-mysqli --with-mysql-sock=/var/run/mysqld/mysqld.sock \
         ${jpg} --with-png-dir=/usr ${openssl} --with-fpm-user=www-data \
-        --with-fpm-group=www-data ${libdir} --enable-ftp --with-imap --with-imap-ssl {$ldap} \
+        --with-fpm-group=www-data ${libdir} --enable-ftp --with-imap ${ldap} --with-imap-ssl \
         --with-kerberos --with-gettext --with-xmlrpc ${webp} --with-xsl \
         --enable-opcache --enable-intl --enable-fpm --with-pear --with-readline ${gmp})
     check_return_code
