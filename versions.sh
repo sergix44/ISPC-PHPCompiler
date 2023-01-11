2dab07517595103279607dbab03d30cb97ce8963
Pietro Marangon <pietro@marangon.me>
2023-01-11 11:49:45 +0100

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -10,10 +10,10 @@ declare -A VERSIONS=(
     ["PHP 7.1.33"]="https://www.php.net/distributions/php-7.1.33.tar.gz"
     ["PHP 7.2.34"]="https://www.php.net/distributions/php-7.2.34.tar.gz"
     ["PHP 7.3.33"]="https://www.php.net/distributions/php-7.3.33.tar.gz"
-    ["PHP 7.4.33"]="https://www.php.net/distributions/php-7.4.32.tar.gz"
-    ["PHP 8.0.26"]="https://www.php.net/distributions/php-8.0.26.tar.gz"
-    ["PHP 8.1.13"]="https://www.php.net/distributions/php-8.1.13.tar.gz"
-    ["PHP 8.2.0RC4"]="https://downloads.php.net/~sergey/php-8.2.0RC4.tar.gz"
+    ["PHP 7.4.33"]="https://www.php.net/distributions/php-7.4.33.tar.gz"
+    ["PHP 8.0.27"]="https://www.php.net/distributions/php-8.0.26.tar.gz"
+    ["PHP 8.1.14"]="https://www.php.net/distributions/php-8.1.14.tar.gz"
+    ["PHP 8.2.1"]="https://www.php.net/distributions/php-8.2.1.tar.gz"
 )
 
 # All PHP versions checksum
@@ -25,7 +25,7 @@ declare -A CHECKSUM=(
     ["PHP 7.2.34"]="8b2777c741e83f188d3ca6d8e98ece7264acafee86787298fae57e05d0dddc78"
     ["PHP 7.3.33"]="9a369c32c6f52036b0a890f290327f148a1904ee66aa56e2c9a7546da6525ec8"
     ["PHP 7.4.33"]="5a2337996f07c8a097e03d46263b5c98d2c8e355227756351421003bea8f463e"
-    ["PHP 8.0.26"]="3c83a7355a640b2ba436b8202e8597df8f9daadee1ec9241729ece8e578d21cd"
-    ["PHP 8.1.13"]="eed1981ce9999d807cb139a9d463ae54bbeda2a57a9a28ad513badf5b99b0073"
-    ["PHP 8.2.0RC4"]="7b344d4763a99d01a13fda253941617fc1f732f9ee1ee1d6caccceb1e74e53e7"
+    ["PHP 8.0.27"]="fe2376faaf91c28ead89a36e118c177f4a8c9a7280a189b97265da1af1f4d305"
+    ["PHP 8.1.14"]="4755af2563ad187ceaf4a3632359c55e3f3be4050e0299e0f713bbb5e0531965"
+    ["PHP 8.2.1"]="6d7b1b8feb14fd1c65a2bc9d0f72c75589a61a946566cf9c3bf9536a5530b635"
 )
