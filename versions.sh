294b335165cfc5624cd3989bc0a401c7e7280939
ressel87 <sr@borndata.dk>
2023-10-27 17:11:51 +0200

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -12,8 +12,8 @@ declare -A VERSIONS=(
     ["PHP 7.3.33"]="https://www.php.net/distributions/php-7.3.33.tar.gz"
     ["PHP 7.4.33"]="https://www.php.net/distributions/php-7.4.33.tar.gz"
     ["PHP 8.0.30"]="https://www.php.net/distributions/php-8.0.30.tar.gz"
-    ["PHP 8.1.24"]="https://www.php.net/distributions/php-8.1.24.tar.gz"
-    ["PHP 8.2.11"]="https://www.php.net/distributions/php-8.2.11.tar.gz"
+    ["PHP 8.1.25"]="https://www.php.net/distributions/php-8.1.25.tar.gz"
+    ["PHP 8.2.12"]="https://www.php.net/distributions/php-8.2.12.tar.gz"
 )
 
 # All PHP versions checksum
@@ -26,6 +26,6 @@ declare -A CHECKSUM=(
     ["PHP 7.3.33"]="9a369c32c6f52036b0a890f290327f148a1904ee66aa56e2c9a7546da6525ec8"
     ["PHP 7.4.33"]="5a2337996f07c8a097e03d46263b5c98d2c8e355227756351421003bea8f463e"
     ["PHP 8.0.30"]="449d2048fcb20a314d8c218097c6d1047a9f1c5bb72aa54d5d3eba0a27a4c80c"
-    ["PHP 8.1.24"]="d6001a5c16765cd1897609fc71ff083e35db9a28c8874a1ff191cdebe80a6460"
-    ["PHP 8.2.11"]="48b1b41279a678a4d4afcd0b256ed921ebf2a91febb0634fdc4449b91c75799f"
+    ["PHP 8.1.25"]="1a8c59d6b3eccb404c229e947558d2bf1220c3dec0b0036690fadc07f39934ab"
+    ["PHP 8.2.12"]="b2b74a91f5fac14ce10ece0ac210f6f5d72f4367a3cb638e80d117d183750a21"
 )
